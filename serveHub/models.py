d1aa5cb19f5c06f7289fdc0eae291de570706e1b
S@m <sam@mas.com>
2026-02-04 15:43:47 +0330

    add category&discount models

--- serveHub/models.py
+++ serveHub/models.py
@@ -1,10 +1,43 @@
 from django.db import models
 
-# Create your models here.
 
-#
-#______________ATTENTION________________
-# tables , product, category for this app
-# product + category --> maryam
-#tables --> agha ibrahim
+class Discount(models.Model):
+    amount = models.IntegerField(
+        verbose_name="Amount",
+        help_text="Amount of Discount"
+    )
+    duration = models.DateTimeField(
+        verbose_name="Duration",
+        help_text="Duration of Discount"
+    )
+    create_date = models.DateTimeField(
+        verbose_name="Create Date",
+        help_text="Create date of discount record"
+    )
+    last_update = models.DateTimeField(
+        verbose_name="Last Update",
+        help_text="Date of discount record Last Update"
+    )
 
+
+class Category(models.Model):
+    type = models.CharField(
+        max_length=30,
+        verbose_name="Type",
+        help_text="Type of Product"
+        )
+    discription = models.TextField(
+        verbose_name="Discription",
+        help_text="Discription of Category"
+    )
+    create_date = models.DateTimeField(
+        verbose_name="Create Date",
+        help_text="Create date of Category"
+    )
+    last_update = models.DateTimeField(
+        verbose_name="Last Update",
+        help_text="Date of Category Last Update"
+    )
+    discount = models.ForeignKey(
+        Discount, on_delete=models.SET_NULL, related_name="category_discount"
+    )
