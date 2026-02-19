09745961a71123f146edc3086b99658f68f1cea9
maryam <army1384m@gmail.com>
2026-02-19 12:00:56 +0330

    make product - reserve table

--- serveHub/admin.py
+++ serveHub/admin.py
@@ -1,13 +1,19 @@
 from django.contrib import admin
-from .models import Product,Category ,Discount , Table
+from .models import Product, Category, Discount, Table, ProductVariant
 
 # Register your models here.
 
+class ProductVariantInline(admin.TabularInline):
+    model = ProductVariant
+    extra = 1
+    verbose_name = "نوع محصول"
+    verbose_name_plural = "انواع محصول"
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price', 'category', 'final_price', 'created_date', 'last_update')
+    list_display = ('name', 'category', 'product_rate', 'created_date', 'last_update')
     list_filter = ('category', 'discount')
-    search_fields = ('name',)
+    search_fields = ('name', 'name_en')
+    inlines = [ProductVariantInline]
 
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Table)
--- serveHub/urls.py
+++ serveHub/urls.py
@@ -12,8 +12,6 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("products/", views.ProductListView.as_view(), name="product_list"),
     path("products/new/", views.ProductCreateView.as_view(), name="product_create"),
-
-
     path(
         "products/<int:id>/edit/",
         views.ProductUpdateView.as_view(),
@@ -27,4 +25,7 @@ urlpatterns = [
     path("tables/", views.TableListView.as_view(), name="table_list"),
     path("tables/new/", views.TableCreateView.as_view(), name="table_create"),
     path("tables/<int:id>/edit/", views.TableUpdateView.as_view(), name="table_update"),
+    
+    path("tables/reserve/", views.TableReservationView.as_view(), name="table_reservation"),
+    path("tables/reserve/<int:table_id>/", views.ReserveTableView.as_view(), name="reserve_table"),
 ]
